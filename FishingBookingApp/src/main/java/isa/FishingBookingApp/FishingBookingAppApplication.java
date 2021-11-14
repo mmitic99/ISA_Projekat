54a0d4e332a07e62d2a0df4081ff0823ce2a1041
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-11-14 12:21:38 +0100

    added backend project

---
+++ FishingBookingApp/src/main/java/isa/FishingBookingApp/FishingBookingAppApplication.java
@@ -0,0 +1,13 @@
+package isa.FishingBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingBookingAppApplication.class, args);
+	}
+
+}
---
+++ FishingBookingApp/src/test/java/isa/FishingBookingApp/FishingBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package isa.FishingBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
