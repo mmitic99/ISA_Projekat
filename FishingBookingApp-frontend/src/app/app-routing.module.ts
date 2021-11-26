324a16106b9223a553c8d293b6310f7b48f993df
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-11-26 15:39:20 +0100

    feat: frontend for home page

--- FishingBookingApp-frontend/src/app/app-routing.module.ts
+++ FishingBookingApp-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./unauthorized-user/unauthorized-user.module').then(mode=>mode.UnauthorizedUserModule)},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
