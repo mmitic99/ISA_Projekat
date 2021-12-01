58d4038e876d949d2ac00e67babecaa79c3f986f
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-01 19:10:43 +0100

    feat: added front for complaint, penalties, deleting account, subscribed entities, with dummy data

---
+++ FishingBookingApp-frontend/src/app/regular-user/request-for-deleting-account/request-for-deleting-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-for-deleting-account',
+  templateUrl: './request-for-deleting-account.component.html',
+  styleUrls: ['./request-for-deleting-account.component.css']
+})
+export class RequestForDeletingAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
