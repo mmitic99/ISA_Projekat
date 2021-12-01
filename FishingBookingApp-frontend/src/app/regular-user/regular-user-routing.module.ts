e74b104df0424eddb02b6bf50eb8372ac2ff7339
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-01 14:33:23 +0100

    fix: deleted home_page component

--- FishingBookingApp-frontend/src/app/regular-user/regular-user-routing.module.ts
+++ FishingBookingApp-frontend/src/app/regular-user/regular-user-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
-import { HomePageComponent } from './home-page/home-page.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'user_profile', component: ProfileComponent },
-  { path: 'home', component: HomePageComponent },
 ];
 
 @NgModule({
