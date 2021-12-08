91571741b35d1ad618c8d1e39814a5aca33b5476
MatijaUskokovic <uskokovic.ra76.2018@uns.ac.rs>
2021-12-08 19:40:56 +0100

    feat: image upload and saving in database

---
+++ FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/repository/EntityImageRepository.java
@@ -0,0 +1,7 @@
+package isa.FishingBookingApp.repository;
+
+import isa.FishingBookingApp.model.EntityImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntityImageRepository extends JpaRepository<EntityImage, Long> {
+}
