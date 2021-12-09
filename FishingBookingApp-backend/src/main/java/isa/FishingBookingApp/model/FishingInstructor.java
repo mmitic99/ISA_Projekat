6f8efdc16840ec8c01a7372c313e8070f5a1d9b7
una12343 <unaobradovic5@gmail.com>
2021-12-09 04:34:19 +0100

    classes added

---
+++ FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/model/FishingInstructor.java
@@ -0,0 +1,39 @@
+package isa.FishingBookingApp.model;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "fishing_instructor_user")
+public class FishingInstructor extends User{
+	
+    @OneToMany(mappedBy = "fishingInstructor", fetch = FetchType.LAZY)
+	private Set<FishingClass> classes;
+	
+	private String explanationOfReg;
+	
+	public FishingInstructor() {
+		setClasses(new HashSet<FishingClass>());
+	}
+	public FishingInstructor(User user) {
+		super(user);
+		setClasses(new HashSet<FishingClass>());
+	}
+	public String getExplanationOfReg() {
+		return explanationOfReg;
+	}
+	public void setExplanationOfReg(String explanationOfReg) {
+		this.explanationOfReg = explanationOfReg;
+	}
+	public Set<FishingClass> getClasses() {
+		return classes;
+	}
+	public void setClasses(Set<FishingClass> classes) {
+		this.classes = classes;
+	}
+
+	
+}
