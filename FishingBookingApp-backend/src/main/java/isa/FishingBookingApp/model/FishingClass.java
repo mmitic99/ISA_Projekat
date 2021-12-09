ea99301be7c4b3eb60a03a86d18074b0d19cc1fe
markovic-dusan <shandusukisuki3@gmail.com>
2021-12-09 04:41:43 +0100

    connecting the right git account

--- FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/model/FishingClass.java
+++ FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/model/FishingClass.java
@@ -5,9 +5,13 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+
+
 @Entity
 public class FishingClass extends ReservationEntities{
 
+	int a ;
 	@ManyToOne(fetch = FetchType.LAZY)
 	private FishingInstructor fishingInstructor;
 }
+//login to dusan git
