842107425dd07b80f80db50e4e0d42198f9d92fa
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-14 14:33:51 +0100

    feat: request for deleting account - front

--- FishingBookingApp-frontend/src/app/regular-user/request-for-deleting-account/request-for-deleting-account.component.ts
+++ FishingBookingApp-frontend/src/app/regular-user/request-for-deleting-account/request-for-deleting-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../service/user.service';
+import { RequestForDeleting } from './RequestForDeleting';
 
 @Component({
   selector: 'app-request-for-deleting-account',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestForDeletingAccountComponent implements OnInit {
 
-  constructor() { }
+  requestForDeleting = new RequestForDeleting("", "");
+
+
+  constructor(private userService: UserService, private toastr:ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  sendRequest(){
+    this.userService.sendRequestForDeletingAccount(this.requestForDeleting).subscribe((data)=>{
+      if(data == null){
+        this.toastr.error("Neuspešno slanje zahteva. Već imate zahteve koji čekaju odobravanje administratora.")
+      }
+      else{
+        this.toastr.success("Uspešno ste poslali zahtev")
+        this.requestForDeleting = new RequestForDeleting("", "");
+      }
+    },
+    (error)=>{
+      this.toastr.success(error)
+    });
   }
 
 }
--- FishingBookingApp-frontend/src/app/regular-user/service/user.service.ts
+++ FishingBookingApp-frontend/src/app/regular-user/service/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { serverPortApi } from 'src/app/app.consts';
+import { RequestForDeleting } from '../request-for-deleting-account/RequestForDeleting';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,14 @@ export class UserService {
         .set('Authorization', `Bearer ${localStorage.getItem('accessToken')}`)
     }
     return this.http.put(serverPortApi + 'users/editUser',editUser, header);
+  }
+  
+  sendRequestForDeletingAccount(requestForDeleting: RequestForDeleting) {
+    var header = {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${localStorage.getItem('accessToken')}`)
+    }
+    requestForDeleting.mailAddress = localStorage.getItem('mailAddress')
+    return this.http.post(serverPortApi + 'users/deletingRequest',requestForDeleting, header);
   }
 }
