d99e11b5ab93dd0c87d4c07b57e0d059244e38ce
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2021-12-17 15:58:36 +0100

    feat: added check for special reservation

--- FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/service/impl/ReservationServiceImpl.java
+++ FishingBookingApp-backend/src/main/java/isa/FishingBookingApp/service/impl/ReservationServiceImpl.java
@@ -4,8 +4,10 @@ import isa.FishingBookingApp.dto.SearchFilterSort;
 import isa.FishingBookingApp.model.AvailableAppointment;
 import isa.FishingBookingApp.model.Reservation;
 import isa.FishingBookingApp.model.ReservationEntities;
+import isa.FishingBookingApp.model.SpecialReservation;
 import isa.FishingBookingApp.repository.AvailableAppointmentRepository;
 import isa.FishingBookingApp.repository.ReservationRepository;
+import isa.FishingBookingApp.repository.SpecialReservationRepository;
 import isa.FishingBookingApp.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,13 @@ import java.util.List;
 @Service
 public class ReservationServiceImpl implements ReservationService {
     private ReservationRepository reservationRepository;
+    private SpecialReservationRepository specialReservationRepository;
     private AvailableAppointmentRepository availableAppointmentRepository;
 
     @Autowired
-    public ReservationServiceImpl(ReservationRepository reservationRepository, AvailableAppointmentRepository availableAppointmentRepository) {
+    public ReservationServiceImpl(ReservationRepository reservationRepository, AvailableAppointmentRepository availableAppointmentRepository, SpecialReservationRepository specialReservationRepository) {
         this.reservationRepository = reservationRepository;
+        this.specialReservationRepository = specialReservationRepository;
         this.availableAppointmentRepository = availableAppointmentRepository;
     }
 
@@ -39,9 +43,28 @@ public class ReservationServiceImpl implements ReservationService {
     }
 
     private boolean reservationEntityIsAvailable(ReservationEntities reservationEntity, SearchFilterSort searchFilterSort) {
-        return DateTimeNotInReservations(reservationEntity, searchFilterSort) && DateTimeInAvailableAppointment(reservationEntity, searchFilterSort);
+        return DateTimeNotInReservations(reservationEntity, searchFilterSort) && DateTimeInAvailableAppointment(reservationEntity, searchFilterSort)
+                && DateTimeNotInSpecialReservations(reservationEntity, searchFilterSort);
     }
 
+    private boolean DateTimeNotInSpecialReservations(ReservationEntities reservationEntity, SearchFilterSort searchFilterSort){
+        List<SpecialReservation> reservations = specialReservationRepository.findByReservationEntityId(reservationEntity.getId());
+
+        LocalDateTime start = searchFilterSort.getDateTime();
+        LocalDateTime end = start.plusDays(searchFilterSort.getDays());
+
+        for (SpecialReservation reservation : reservations) {
+            LocalDateTime startReservation = reservation.getStart();
+            LocalDateTime endReservation = startReservation.plusHours(Double.valueOf(reservation.getDurationInHours()).longValue());
+            if((start.isBefore(startReservation) && end.isBefore(startReservation)) || (start.isAfter(endReservation) && end.isAfter(endReservation))){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
     private boolean DateTimeNotInReservations(ReservationEntities reservationEntity, SearchFilterSort searchFilterSort){
         List<Reservation> reservations = reservationRepository.findByReservationEntityId(reservationEntity.getId());
 
@@ -70,7 +93,7 @@ public class ReservationServiceImpl implements ReservationService {
         for (AvailableAppointment availableAppointment : availableAppointments) {
             LocalDateTime startAvailableAppointment = availableAppointment.getFromTime();
             LocalDateTime endAvailableAppointment = availableAppointment.getToTime();
-            
+
             if((startAvailableAppointment.isBefore(start) || startAvailableAppointment.isEqual(start))
                     && (endAvailableAppointment.isAfter(end) || endAvailableAppointment.isEqual(end))){
                 return true;
