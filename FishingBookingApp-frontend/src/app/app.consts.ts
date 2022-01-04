4fa816c7a78d741c9fd3446e11b4840ff64041cf
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-01-04 12:41:12 +0100

    heroku deploy

--- FishingBookingApp-frontend/src/app/app.consts.ts
+++ FishingBookingApp-frontend/src/app/app.consts.ts
@@ -1,2 +1,4 @@
-export const serverPortApi = 'http://localhost:8080/api/'
-export const serverPortAuth = 'http://localhost:8080/auth/'
+import { environment } from "src/environments/environment"
+
+export const serverPortApi = environment.serverPort + 'api/'
+export const serverPortAuth = environment.serverPort + 'auth/'
