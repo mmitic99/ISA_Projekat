51d3896c815597ea93c66e3e287a29eed61f2e27
MatijaUskokovic <uskokovic.ra76.2018@uns.ac.rs>
2022-01-08 16:55:34 +0100

    test: integration tests

---
+++ FishingBookingApp-backend/src/test/java/isa/FishingBookingApp/controller/ReservationEntitiesControllerTest.java
@@ -0,0 +1,60 @@
+package isa.FishingBookingApp.controller;
+
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+
+import isa.FishingBookingApp.constants.ReservationEntitiesConstants;
+import isa.FishingBookingApp.dto.CottageDTO;
+import isa.FishingBookingApp.model.Cottage;
+import isa.FishingBookingApp.util.TestUtil;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ReservationEntitiesControllerTest {
+    private static final String URL_PREFIX = "/api/reservationEntities";
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    // Student 2
+    @Test
+    @Transactional
+    @Rollback(true)
+    public void testGetAdditionalServices() throws Exception {
+        mockMvc.perform(get(URL_PREFIX + "/additionalServices/" + ReservationEntitiesConstants.DB_ID_RESERVATION_ENTITY_ONE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(ReservationEntitiesConstants.DB_ADDITIONAL_SERVICES_COUNT_OF_ENTITY_ONE)));
+    }
+}
---
+++ FishingBookingApp-backend/src/test/java/isa/FishingBookingApp/service/BusinessReportServiceTest.java
@@ -0,0 +1,34 @@
+package isa.FishingBookingApp.service;
+
+import isa.FishingBookingApp.dto.IncomeFromEntityDTO;
+import isa.FishingBookingApp.model.ReservationEntities;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BusinessReportServiceTest {
+
+    @InjectMocks
+    private BusinessReportService businessReportService;
+/*
+    @Test
+    public void testFindAll() {
+
+        /*
+        List<IncomeFromEntityDTO> incomes = businessReportService.getIncomeReportForOwnerInTimeRange();
+
+        List<ReservationEntities> re = reservationEntitiesService.getAll();
+        assertThat(re).hasSize(2);
+
+        // 3. Verifikacija: asertacije i/ili verifikacija interakcije sa mock objektima
+        assertThat(students).hasSize(1);
+        assertEquals(students.get(0).getFirstName(), DB_FIRST_NAME.toUpperCase());
+
+
+    }
+
+ */
+}
